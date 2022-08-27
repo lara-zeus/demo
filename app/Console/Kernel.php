01271fff85cdac472a17252b6cb3482d4fae650e
Ashraf Monshi <wh7r.com@gmail.com>
2022-08-27 14:48:45 +0300

    wip

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        // art migrate:fresh --seed
         $schedule->command('migrate:fresh --force --seed --quiet')->daily();
     }
 
