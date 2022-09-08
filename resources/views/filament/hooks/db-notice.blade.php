ad6a5875f6f96acfaef7ec13df09743c0a688d0d
Ashraf Monshi <wh7r.com@gmail.com>
2022-09-08 03:22:11 +0300

    wip

--- resources/views/filament/hooks/db-notice.blade.php
+++ resources/views/filament/hooks/db-notice.blade.php
@@ -1,4 +1,4 @@
-<div class="bord bg-amber-100 w-full px-3 py-4 m-2 mt-0 rounded-lg shadow-md hover:shadow-lg duration-200 transition ease-in-out flex items-center justify-start gap-2">
+<div class="bg-amber-100 w-full px-3 py-4 m-2 mt-0 rounded-lg shadow-md hover:shadow-lg duration-200 transition ease-in-out flex items-center justify-start gap-2">
     <x-heroicon-o-exclamation class="w-6 h-6 text-amber-500" />
     <span>
         the database will reset every day
