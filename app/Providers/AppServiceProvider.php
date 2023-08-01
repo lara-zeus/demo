f2ee20c7fb8ce5132809584b59e120928bf6d5bc
Ash Monsh <wh7r.com@gmail.com>
2023-08-02 01:01:49 +0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
 
         // I know! 🤷🏽‍, please let me have my fun!!!
         Blade::directive('stillCode', function () {
-            return '<span class="title-font"><span class="title-font rounded-lg bg-gray-200 italic px-2 dark:bg-gray-500"><span class="title-font not-italic">Still ~ </span><span class="title-font not-italic text-purple-500">&lt;?</span><span> code</span><span class="title-font blink-cursor not-italic font-thin text-gray-400">|</span><span class="title-font not-italic text-purple-500">?&gt;</span></span></span>';
+            return '<span class="font-courier"><span class="font-courier rounded-lg bg-gray-200 italic px-2 dark:bg-gray-500"><span class="font-courier not-italic">Still ~ </span><span class="font-courier not-italic text-purple-500">&lt;?</span><span> code</span><span class="font-courier blink-cursor not-italic font-thin text-gray-400">|</span><span class="font-courier not-italic text-purple-500">?&gt;</span></span></span>';
         });
 
         Blade::directive('zeus', function ($part = null) {
@@ -33,7 +33,7 @@ class AppServiceProvider extends ServiceProvider
 
         Blade::directive('stillStats', function ($code) {
             if (!app()->isLocal()) {
-                return '<!-- stats --><script async defer data-website-id="'.$code.'" src="https://stats.code.com/umami.js"></script>';
+                return '<!-- stats --><script async defer data-website-id="'.$code.'" src="https://stats.still-code.com/script.js"></script>';
             }
 
             return '<!-- no tags for you -->';
