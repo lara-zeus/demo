a38eb79c6537a414b6c842bb22a204c45fe32f96
Ash Monsh <wh7r.com@gmail.com>
2023-08-02 02:55:07 +0300

    Update lang-switcher.blade.php

--- resources/views/filament/hooks/lang-switcher.blade.php
+++ resources/views/filament/hooks/lang-switcher.blade.php
@@ -1,6 +1,7 @@
 <div class="relative ml-4 gap-4 items-center justify-center flex">
     <div x-tooltip.raw="Like the project? consider sponsoring me 😇">
         <iframe src="https://github.com/sponsors/atmonshi/button"
+                loading="lazy"
                 title="Sponsor atmonshi" height="32" width="114"
                 style="border: 0; border-radius: 6px;"></iframe>
     </div>
