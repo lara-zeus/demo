3bae6837cca856ebb0dded9e3271c5aa832e5806
Ash Monsh <wh7r.com@gmail.com>
2023-09-13 00:45:49 +0300

    wip

--- config/zeus.php
+++ config/zeus.php
@@ -19,7 +19,7 @@ return [
      * you can use the default layout as a starting point for your blog.
      * however, if you're already using your own component, just set the path here.
      */
-    'layout' => 'zeus::themes.zeus.layouts.app',
+    'layout' => 'zeus::components.app',
 
     /**
      * the default theme, for now we only have one theme, and soon we will provide more free and premium themes.
