1804119b3cb7ca2ab9ddff1137ccf309edbf8b01
Ash Monsh <wh7r.com@gmail.com>
2023-10-10 06:45:56 +0300

    wip

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/pre', \LaraZeus\BoltPreset\Livewire\Preset::class);
-
 Route::view('/', 'welcome');
 Route::redirect('/login', '/admin/login')->name('login');
 
