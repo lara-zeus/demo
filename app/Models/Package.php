8fd13337a9d9f03d427b9e57e829ec4ea3541552
Ash Monsh <wh7r.com@gmail.com>
2023-11-10 14:49:54 +0300

    wip

--- app/Models/Package.php
+++ app/Models/Package.php
@@ -24,7 +24,7 @@ class Package extends Model
                         [
                             'text' => 'Bolt Pro',
                             'url' => 'https://larazeus.com/bolt-pro',
-                        ]
+                        ],
                     ],
                 ]),
             ],
