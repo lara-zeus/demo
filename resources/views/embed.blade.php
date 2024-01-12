d37266129f8de2e54b21c935fea39c270d34bba2
Ash Monsh <wh7r.com@gmail.com>
2024-01-13 00:54:49 +0300

    wip

--- resources/views/embed.blade.php
+++ resources/views/embed.blade.php
@@ -1,15 +1,19 @@
 <x-app>
     <div class="container mx-auto my-4 space-y-6">
-        <div>
-            any text
-        </div>
-        <div class="w-1/3 mx-auto">
-            @php
-                $form = \LaraZeus\Bolt\Models\Form::first()
-            @endphp
-            @if($form !== null)
-                <livewire:bolt.fill-form :slug="$form->slug" :inline="true"/>
-            @endif
+        <div class="flex gap-2">
+            <div class="w-1/2 mx-auto">
+                <h3>Embed with LW</h3>
+                @php
+                    $form = \LaraZeus\Bolt\Models\Form::first()
+                @endphp
+                @if($form !== null)
+                    <livewire:bolt.fill-form :slug="$form->slug" :inline="true"/>
+                @endif
+            </div>
+            <div class="w-1/2 mx-auto">
+                <h3>Embed with iframe</h3>
+                <iframe class="bord" height="500" src="https://larazeus.com/bolt/embed/{{ $form->slug }}"></iframe>
+            </div>
         </div>
         <div>
             any text
