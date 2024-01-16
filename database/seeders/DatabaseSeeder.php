f501048f695296a13236f46e44c436fe22f5968f
Ash Monsh <wh7r.com@gmail.com>
2024-01-16 14:39:15 +0300

    wip

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -37,6 +37,7 @@ class DatabaseSeeder extends Seeder
             RainSeeder::class,
             HermesSeeder::class,
             BoltSectionsSeeder::class,
+            SelectTreeSeeder::class,
         ]);
     }
 }
