9fa9be1c63d786338d76a26197e91e5decfced84
Ash Monsh <wh7r.com@gmail.com>
2024-06-14 20:42:41 +0300

    Update CarResource.php

--- app/Filament/Resources/CarResource.php
+++ app/Filament/Resources/CarResource.php
@@ -36,8 +36,6 @@ class CarResource extends Resource
             ->columns([
                 Tables\Columns\TextColumn::make('name')
                     ->searchable(),
-                Tables\Columns\TextColumn::make('compeny.name')
-                    ->searchable(),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
