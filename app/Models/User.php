0c73b79b052c6d2ca0b1cae201fc4aab3f9065c2
Ash Monsh <wh7r.com@gmail.com>
2024-08-14 14:05:20 +0300

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -91,7 +91,7 @@ class User extends Authenticatable implements FilamentUser, HasAvatar
     public function avatarUrl(): Attribute
     {
         return new Attribute(
-            get: fn () => 'https://larazeus.com/avatars/'. urlencode($this->name).'.svg'
+            get: fn () => 'https://larazeus.com/avatars/'. urlencode($this->name).'/30.svg'
         );
     }
 }
