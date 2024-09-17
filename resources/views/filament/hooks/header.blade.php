14c9090925b1202942e3e0bdb8fcfcb2f98ded94
Ash Monsh <wh7r.com@gmail.com>
2024-09-17 22:44:48 +0300

    wip

--- resources/views/filament/hooks/header.blade.php
+++ resources/views/filament/hooks/header.blade.php
@@ -1,1 +1,22 @@
-ss
+<style>
+    #Durchblick-toggleFeedbackMode{
+        left: unset !important;
+        right: 20px !important;
+    }
+</style>
+<script src="https://durchblick.app/widget.js"></script>
+<script>
+    DurchblickWidget.init({
+        apiKey: '7354e9b0e9dfe06a22617f8d',                 // required
+        publicFeedback: true,                      // optional, default: false (add ?allowFeedback=1 to the URL to enable)
+        primaryColor: '#333',                       // optional, default: #333
+        secondaryColor: '#ccc',                     // optional, default: #ccc
+        dotSize: '20px',                            // optional, default: 20px
+        dotColor: '#f00',                           // optional, default: red
+        enterFeedbackModeText: 'Feedback Mode Off', // optional, default: 'Enter feedback mode'
+        exitFeedbackModeText: 'Feedback Mode On',   // optional, default: 'Exit feedback mode'
+    });
+</script>
+
+
+
