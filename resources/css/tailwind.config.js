330067c0bb88bb63f80c3a50677a19d42c5689a9
Ash Monsh <wh7r.com@gmail.com>
2025-03-02 19:46:46 +0300

    add replies to tailwind css config

--- resources/css/tailwind.config.js
+++ resources/css/tailwind.config.js
@@ -16,6 +16,8 @@ module.exports = {
         './vendor/lara-zeus/wind/src/Filament/Resources/LetterResource.php',
         './vendor/lara-zeus/wind/src/Livewire/ContactsForm.php',
 
+        './vendor/lara-zeus/replies/resources/views/**/*.blade.php',
+
         './vendor/lara-zeus/sky/resources/views/**/*.blade.php',
         './vendor/lara-zeus/sky/src/Models/PostStatus.php',
 
