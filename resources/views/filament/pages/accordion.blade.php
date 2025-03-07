d9a4a627609edd48be6a89bf8e8a116cf50f01ab
Ash Monsh <wh7r.com@gmail.com>
2025-03-07 22:05:20 +0300

    fixes

--- resources/views/filament/pages/accordion.blade.php
+++ resources/views/filament/pages/accordion.blade.php
@@ -14,7 +14,7 @@
         <x-zeus-accordion::accordion activeAccordion="1">
             <x-zeus-accordion::accordion.item
                 :label="__('Contact Us')"
-                icon="clarity-contract-line"
+                icon="tabler-brand-pagekit"
             >
                 <div class="dark:bg-gray-900 bg-white p-4 *:py-2">
                     <p>title</p>
@@ -24,7 +24,7 @@
 
             <x-zeus-accordion::accordion.item
                 :label="__('Call Us')"
-                icon="clarity-envelope-line"
+                icon="tabler-mail"
                 badge="New Item"
                 badgeColor="danger"
             >
@@ -37,7 +37,7 @@
 
             <x-zeus-accordion::accordion.item
                 :label="__('Find Us')"
-                icon="clarity-map-outline-badged"
+                icon="tabler-map-2"
             >
                 <div class="dark:bg-gray-900 items-center justify-center bg-white p-4 *:py-2">
                     <p>map</p>
