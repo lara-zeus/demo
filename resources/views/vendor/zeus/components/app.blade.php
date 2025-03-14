44d468a0cfc0779df032c080b9e33bc93a0c03f9
Ash Monsh <wh7r.com@gmail.com>
2025-03-14 21:27:42 +0300

    fix and update

--- resources/views/vendor/zeus/components/app.blade.php
+++ resources/views/vendor/zeus/components/app.blade.php
@@ -14,6 +14,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Almarai:wght@300;400;700;800&family=KoHo:ital,wght@0,200;0,300;0,500;0,700;1,200;1,300;1,600;1,700&display=swap" rel="stylesheet">
 
+    @livewireStyles
     @filamentStyles
     @stack('styles')
     @vite('resources/css/app.css')
