ff382c1524cd5fbb36dc4442fa99370697bf3838
Ash Monsh <wh7r.com@gmail.com>
2025-03-17 23:17:56 +0300

    add flags

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,6 @@ export default defineConfig({
             'resources/css/daisy.css',
             'resources/css/another-portfolio.css',
             'resources/css/flag-icons.css',
-
             'resources/css/filament/admin/theme.css',
             'resources/css/filament/guests/theme.css',
         ]),
