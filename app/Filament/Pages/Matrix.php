95cc4cb6c19e5842e442e6ed9accfabb57ce0e7e
Ash Monsh <wh7r.com@gmail.com>
2025-04-05 13:40:49 +0300

    wip

--- app/Filament/Pages/Matrix.php
+++ app/Filament/Pages/Matrix.php
@@ -28,6 +28,11 @@ class Matrix extends Page
 
     public ?string $qrcode;
 
+    public function mount(): void
+    {
+        $this->form->fill();
+    }
+
     protected function getHeaderActions(): array
     {
         return [
@@ -45,7 +50,12 @@ class Matrix extends Page
                     ->schema([
                         MatrixAlias::make('options')
                             // ->disabled()
-                            ->disableOptionWhen(fn (string $value): bool => $value === 'm' || $value === 'p' || $value === 'users')
+                            ->formatStateUsing(fn() => [
+                                'companies' => ['c' => true],
+                                'clients' => ['m' => true, 'p' => true],
+                            ])
+                            ->disableOptionWhen(fn(string $value
+                            ): bool => $value === 'm' || $value === 'p' || $value === 'users')
                             ->rowSelectRequired(false)
                             ->helperText('you can disable any options, like in the users row, the Manage and Approve are disabled')
                             ->label('Resources Operations')
@@ -65,9 +75,8 @@ class Matrix extends Page
                             ]),
 
                         MatrixAlias::make('question1')
-                            ->disableOptionWhen(fn (string $value): bool => $value === 0)
+                            ->disableOptionWhen(fn(string $value): bool => $value === 0)
                             ->rowSelectRequired(false)
-
                             ->label('Tell us about your mod')
                             ->asCheckbox()
                             ->columnData([
