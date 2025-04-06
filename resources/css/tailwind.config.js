6ab663b63b158215b3aa1ba7afe80428e42c2956
Ash Monsh <wh7r.com@gmail.com>
2025-04-06 13:55:29 +0300

    Update tailwind.config.js

--- resources/css/tailwind.config.js
+++ resources/css/tailwind.config.js
@@ -110,27 +110,5 @@ module.exports = {
             }
         },
     },
-    plugins: [
-        /*plugin(function ({addUtilities, addComponents, e, config}) {
-            const sketchyBorders = {
-                '.border-sketchy-sm': {
-                    borderRadius: '255px 25px 225px 25px/25px 225px 25px 255px',
-                    transition: 'all 0.3s ease-in-out'
-                },
-                '.border-sketchy-md': {
-                    borderRadius: '25px 55px 10px 45px/85px 20px 55px 20px',
-                    transition: 'all 0.3s ease-in-out'
-                },
-                '.border-sketchy-lg': {
-                    borderRadius: '5px 55px 25px 25px/85px 20px 55px 20px',
-                    transition: 'all 0.3s ease-in-out'
-                },
-            }
-
-            addUtilities(sketchyBorders, {
-                variants: ['responsive', 'hover'],
-            })
-        }),*/
-        /*require('tailwindcss-debug-screens'),*/
-    ],
+    plugins: [],
 }
