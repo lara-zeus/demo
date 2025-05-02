28f7282da8967c64bbb049bfff14f14331063af9
Ash Monsh <wh7r.com@gmail.com>
2025-05-03 01:05:22 +0300

    add mark

--- app/Filament/Pages/Mark.php
+++ app/Filament/Pages/Mark.php
@@ -38,20 +38,52 @@ class Mark extends Page
                 TextInput::make('name'),
                 MarkForm::make('likes')
                     ->label('Like')
+                    ->colors([
+                        true => 'green',
+                        false => 'secondary',
+                    ])
                     ->like(),
-                MarkForm::make('user_fav')
-                    ->helperText('user fav')
+                MarkForm::make('user_bookmark')
+                    ->helperText('user Bookmark?')
                     ->label('Bookmark')
+                    ->colors([
+                        true => 'info',
+                    ])
                     ->bookMark(),
+
+                MarkForm::make('user_fav')
+                    ->helperText('or user Favorite?')
+                    ->label('Favorite')
+                    ->colors([
+                        true => 'violet',
+                    ])
+                    ->icons([
+                        true => 'heroicon-o-star',
+                    ])
+                    ->selectedIcons([
+                        true => 'heroicon-s-star',
+                    ]),
+
                 MarkForm::make('user_rate')
+                    ->default(3)
+                    ->colors([
+                        1 => 'yellow',
+                        2 => 'yellow',
+                        3 => 'yellow',
+                        4 => 'yellow',
+                        5 => 'yellow',
+                    ])
                     ->label('Rating')
                     ->rating(),
-
 
                 MarkForm::make('user_mode')
                     ->icons([
                         true => 'tabler-mood-smile',
                         false => 'tabler-mood-sad',
+                    ])
+                    ->colors([
+                        true => 'sky',
+                        false => 'rose',
                     ])
                     ->selectedIcons([
                         true => 'tabler-mood-smile-filled',
