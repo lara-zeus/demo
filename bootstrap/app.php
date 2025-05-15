c3029f3815df14072af23ff8af03f059cc23d381
Shift <shift@laravelshift.com>
2025-05-15 15:30:07 +0000

    Re-register HTTP middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,21 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectUsersTo(RouteServiceProvider::HOME);
+
+        $middleware->append([
+            \App\Http\Middleware\SetLang::class,
+            \App\Http\Middleware\SetTheme::class,
+            \Torchlight\Middleware\RenderTorchlight::class,
+        ]);
+
+        $middleware->throttleApi();
+
+        $middleware->replace(\Illuminate\Http\Middleware\TrustProxies::class, \App\Http\Middleware\TrustProxies::class);
+
+        $middleware->alias([
+            'auth' => \App\Http\Middleware\Authenticate::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
