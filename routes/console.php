3114f82b1ff5c1377a6702407d0de62c4a8b397f
Shift <shift@laravelshift.com>
2025-05-15 15:30:10 +0000

    Re-register scheduled commands

--- routes/console.php
+++ routes/console.php
@@ -1,1 +1,12 @@
 <?php
+
+use Illuminate\Support\Facades\Schedule;
+
+
+Schedule::command('migrate:fresh --seed --force --quiet')
+    ->daily()
+    ->at('4:00');
+
+Schedule::command('php artisan seo:scan --quiet')
+    ->weekly()
+    ->at('5:00');
