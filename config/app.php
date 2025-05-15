0c0d73ab69a7f45b04c7e63dc8f2e23eddc3033c
Shift <shift@laravelshift.com>
2025-05-15 15:30:08 +0000

    Re-register service providers

--- config/app.php
+++ config/app.php
@@ -45,25 +45,5 @@ return [
         // 'uranus',
     ],
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Laravel Framework Service Providers...
-         */
-
-        /*
-         * Package Service Providers...
-         */
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\Filament\AdminPanelProvider::class,
-        App\Providers\Filament\GuestsPanelProvider::class,
-        App\Providers\RouteServiceProvider::class,
-    ])->toArray(),
 
 ];
