147e8b589208b06770f2616cff00a6893f0210ba
Ash Monsh <wh7r.com@gmail.com>
2025-08-12 20:42:07 +0300

    tailwind css try 5

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel([
-            'resources/js/another-portfolio.js',
             'resources/css/app.css',
-            'resources/css/daisy.css',
-            'resources/css/another-portfolio.css',
             'resources/css/flag-icons.css',
             'resources/css/filament/admin/theme.css',
         ]),
